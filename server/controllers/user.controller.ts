9121883decbd6632b356145aa70ea0f1421dc547
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-10-22 23:50:19 +0600

    create user controller and ejs email template

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -4,6 +4,9 @@ import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import userModel from "../models/user.model";
 import jwt, { Secret } from "jsonwebtoken";
+import ejs from "ejs";
+import path from "path";
+import sendMail from "../utils/sendMail";
 
 //register user
 interface TRegisterBody {
@@ -27,6 +30,28 @@ export const registerUser = CatchAsyncError(
       const user: TRegisterBody = { name, email, password };
 
       const activationToken = CreateActivationToken(user);
+      const activationCode = activationToken.activationCode;
+      const data = { user: { name: user.name }, activationCode };
+
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/activation-mail.ejs"),
+        data
+      );
+      try {
+        await sendMail({
+          email: user.email,
+          subject: "Activate your account",
+          template: "activation-mail.ejs",
+          data,
+        });
+        res.status(201).json({
+          success: true,
+          message: `Please check your ${user.email} to activate your account`,
+          activationToken: activationToken.token,
+        });
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+      }
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
