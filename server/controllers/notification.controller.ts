782cd27c52932d12a278b7c2bd9e08a7d27fe0b8
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-10-26 01:38:42 +0600

    delete notificatioon by cron job auti sceduling

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import notificationModel from "../models/notification.model";
+import cron from "node-cron";
 
 //get all notifications -(only for admin)
 export const getNotifications = CatchAsyncError(
@@ -45,3 +46,13 @@ export const updateNotification = CatchAsyncError(
     }
   }
 );
+
+//delete notification by cronjob
+cron.schedule("0 0 0 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await notificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+  console.log("Deleted 30 days ago read notifications");
+});
